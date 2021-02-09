5a5a1ccf7e2ac628244846a4b5da8d19d8f91b70
Krzysztof Szczypior <s23578-pj>
2021-02-09 22:38:34 +0100

    ustrukturyzowanie folderow z zajec

---
+++ s23578-pj/zajecia5/program.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	for (int i = 0; i < 10; i++)
+
+	{
+		printf("+---------------------+\n");
+		printf("|                            |\n");
+		printf("|    Liczę: $d     |\n", i);
+		printf("|                            |\n");
+		printf("+---------------------+\n");
+	}
+	return 0;
+}
